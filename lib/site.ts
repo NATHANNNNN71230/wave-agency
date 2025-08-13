aac6e054c3430028b04868ca7fc45b74b53c6333
NATHANNNNN71230 <168438466+NATHANNNNN71230@users.noreply.github.com>
2025-08-13 10:03:47 +0200

    Update site.ts

--- lib/site.ts
+++ lib/site.ts
@@ -1,6 +1,6 @@
 export const site = {
   name: "Wave Agency",
-  domain: "https://wave-agency-aqsx-2m637kave-nathans-projects-1efdf863.vercel.app",
+  domain: "https://wave-agency-aqsx-2m637kave-nathans-projects-1efdf863.vercel.app/",
   email: "waveagency.booking@gmail.com",
   phone: "+33744266719",
   instagram: "https://instagram.com/wave.agencyyy",
